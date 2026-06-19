a7db730b8dc520c406035e9df3867faa0500b9db
Vidied <davidepan2001@gmail.com>
2026-06-19 13:19:43 +0200

    catalogo finito

--- src/main/java/entities/Catalogo.java
+++ src/main/java/entities/Catalogo.java
@@ -1,3 +1,5 @@
+package entities;
+
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -24,5 +26,71 @@ public abstract class  Catalogo {
     @Column(nullable = false,  name = "numero_pagine")
     private int numeroPagine;
 
-    
+
+
+
+    public Catalogo (){}
+
+    public Catalogo(String titolo, Long isbn, LocalDate annoPub, int numeroPagine) {
+        this.titolo = titolo;
+        this.isbn = isbn;
+        this.annoPub = annoPub;
+        this.numeroPagine = numeroPagine;
+    }
+
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDate getAnnoPub() {
+        return annoPub;
+    }
+
+    public void setAnnoPub(LocalDate annoPub) {
+        this.annoPub = annoPub;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Catalogo{" +
+                "id=" + id +
+                ", titolo='" + titolo + '\'' +
+                ", isbn=" + isbn +
+                ", annoPub=" + annoPub +
+                ", numeroPagine=" + numeroPagine +
+                '}';
+    }
 }
