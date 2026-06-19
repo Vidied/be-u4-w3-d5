4ceb454577f1699caf777e9f52abf7a4baedd9ea
Vidied <davidepan2001@gmail.com>
2026-06-19 16:16:55 +0200

    query per find autore, anno e titolo

--- src/main/java/dao/CatalogoDao.java
+++ src/main/java/dao/CatalogoDao.java
@@ -4,6 +4,9 @@ import entities.Catalogo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class CatalogoDao {
 
     private final EntityManager em;
@@ -49,5 +52,24 @@ public class CatalogoDao {
         } else {
             System.out.println("Nessun elemento con ISBN: " + isbn + " è stato trovato!");
         }
+    }
+
+    public List<Catalogo> findByAnno (int anno) {
+        return em.createQuery("SELECT c FROM Catalogo c WHERE EXTRACT(YEAR FROM c.annoPub) = :anno", Catalogo.class)
+                .setParameter("anno", anno)
+                .getResultList();
+    }
+    
+    public List<Catalogo> findByTitolo(String titolo){
+        //LOWER per ovviamente rendere il testo case insensitive rendendo tutto minuscolo
+        return em.createQuery("SELECT c FROM Catalogo c WHERE LOWER(c.titolo) LIKE :titolo", Catalogo.class)
+                .setParameter("titolo","%" + titolo + "%")
+                .getResultList();
+    }
+    //Siccome autore è presente solo in libro cerco direttamente in libro
+    public List<Catalogo> findByAutore(String autore){
+        return em.createQuery("SELECT c FROM Libro c WHERE c.autore = :autore", Catalogo.class)
+                .setParameter("autore", autore)
+                .getResultList();
     }
 }
