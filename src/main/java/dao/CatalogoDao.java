41d176edaf60dc17f4f84bb74f6b2a8c0d9a9c72
Vidied <davidepan2001@gmail.com>
2026-06-19 15:27:16 +0200

    find save e remove by isbn in catalogo

---
+++ src/main/java/dao/CatalogoDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import entities.Catalogo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class CatalogoDao {
+
+    private final EntityManager em;
+
+    public CatalogoDao(EntityManager em){
+        this.em = em;
+    }
+
+
+
+    public void save(Catalogo e) {
+        EntityTransaction t = em.getTransaction();
+
+
+        t.begin();
+
+
+        em.persist(e);
+
+
+        t.commit();
+        System.out.println("Elemento: " + e.getTitolo() + " con ISBN: " + e.getIsbn() +" è stato salvato con successo nel DB");
+    }
+
+    public Catalogo findByIsbn(Long isbn){
+        return em.createQuery("SELECT c FROM Catalogo c WHERE c.isbn = :isbn", Catalogo.class)
+                .setParameter("isbn", isbn)
+                .getSingleResult();
+    }
+
+    public void findAndRemoveByIsbn(Long isbn){
+        Catalogo trovato = findByIsbn(isbn);
+
+        if(trovato !=null) {
+            EntityTransaction t = em.getTransaction();
+
+            t.begin();
+
+            em.remove(trovato);
+
+            t.commit();
+            System.out.println("Elemento con ISBN: " + isbn + " è stato rimosso con successo dal DB");
+        } else {
+            System.out.println("Nessun elemento con ISBN: " + isbn + " è stato trovato!");
+        }
+    }
+}
